8732bb0db605a160a9a8c09723ac29eae82a6c3e
GayanPradeep97 <gayanpradeep0212@gmail.com>
2023-04-04 17:59:26 +0530

    update

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NoteService } from '../note.service';
 import { Note } from '../note'
 import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
@@ -11,9 +11,13 @@ import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 
 export class NoteComponent implements OnInit {
 
+
   noteForm !: FormGroup;
+  editForm !: FormGroup;
 
   notesData: any=[];
+
+  notesDetails : any=[];
 
   noteObj : Note = {
     id:'',
@@ -26,13 +30,20 @@ export class NoteComponent implements OnInit {
       this.noteForm = this.formBuilder.group({
         title : ['',Validators.required],
         description:['', Validators.required]
+      });
+
+      this.editForm = this.formBuilder.group({
+        edit_title : ['',Validators.required],
+        edit_description:['', Validators.required]
       })
     }
 
   ngOnInit(){
+
     this.getAllNotes();
   }
 
+  //Add data to firebase
   addNote(){
     const {value} = this.noteForm;
     this.noteObj.id= '';
@@ -49,12 +60,44 @@ export class NoteComponent implements OnInit {
      
     })
   }
-
+  //get data into display
   getAllNotes(){
     this.noteServics.getNotes().subscribe((res:Note[]) =>{
       console.log(res);
       this.notesData = res;
     })
+  }
+
+  //delete data
+  deleteNote(note: Note){
+    let decision = confirm('Are sure want to delete this Note ?');
+    if(decision == true){
+      this.noteServics.deleteNote(note);
+    }
+  }
+
+  //edit button onclick function
+  getAllDtails(note:Note){
+    this.notesDetails = note;
+    console.log(this.notesDetails)
+    
+  }
+
+  //Updatee data
+  updateNote(note:Note){
+    const {value} = this.editForm
+    console.log(value);
+
+    (this.noteObj.id = note.id);
+    (this.noteObj.note_title = value.edit_title);
+    (this.noteObj.note_des = value.edit_description);
+
+    this.noteServics.updateNote(note, this.noteObj).then(()=>{
+      alert('note updated successfull!')
+     
+    })
+    this.editForm.reset();
+
   }
 
   
